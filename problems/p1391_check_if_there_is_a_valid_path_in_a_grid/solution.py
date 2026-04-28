3f80242cfbcc534a0354b2004e10731d0411b51b
Benedikt Mews <Benedikt.Mews@gmail.com>
2026-04-28 08:52:09 +0200

    solved 1391

--- problems/p1391_check_if_there_is_a_valid_path_in_a_grid/solution.py
+++ problems/p1391_check_if_there_is_a_valid_path_in_a_grid/solution.py
@@ -10,35 +10,39 @@ class Solution:
         visited = [[False] * m for _ in range(n)]
         directions = ((0, 1), (1, 0), (-1, 0), (0, -1))
         layouts = {
-            1: ('l', 'r'),
-            2: ('u','d'),
-            3: ('l','d'),
-            4: ('r','d'),
-            5: ('l','u'),
-            6: ('r','u'),
+            1: ((0, -1), (0, 1)),
+            2: ((-1, 0),(1, 0)),
+            3: ((0, -1),(1, 0)),
+            4: ((0, 1),(1, 0)),
+            5: ((0, -1),(-1, 0)),
+            6: ((0, 1),(-1, 0)),
         }
 
         def in_bounds(ni: int, nj: int) -> bool:
             return 0 <= ni < n and 0 <= nj < m
         
-        def suitingLayout(ni, nj, current_layout):
-            next_layout = layouts[grid[ni][nj]]
-            return bool(set(current_layout) & set(next_layout))
-        
-        def can_visit(ni: int, nj: int, current_layout) -> bool:
-            return in_bounds(ni, nj) and not visited[ni][nj] and suitingLayout(ni, nj, current_layout)
+        def suitingLayout(i, j, di, dj):
+            current_layout = layouts[grid[i][j]]
+            next_layout = layouts[grid[i+di][j+dj]]
 
+            return (di, dj) in current_layout and (-di, -dj) in next_layout
+        
+        def can_visit(i: int, j: int, di: int, dj: int) -> bool:
+            return (
+                in_bounds(i+di, j+dj) 
+                and not visited[i+di][j+dj] 
+                and suitingLayout(i, j, di, dj)
+                )
 
         def dfs(i, j):
             visited[i][j] = True
             if i == n - 1 and j == m - 1:
                 return True
 
-            current_layout = layouts[grid[i][j]]
             nexts = [
                 (i + di, j + dj)
                 for di, dj in directions
-                if can_visit(i + di, j + dj, current_layout)
+                if can_visit(i, j, di, dj)
             ]
 
             return any(dfs(ni, nj) for ni, nj in nexts)
